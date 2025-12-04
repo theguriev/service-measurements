f2e46486ba26cbc631bdc6f0a41aa8575374c292
biguard1 <bogdasha05@gmail.com>
2025-12-04 17:30:56 +0200

    feat: authentication remake

--- server/utils/getUserRole.ts
+++ server/utils/getUserRole.ts
@@ -1,7 +1,7 @@
 import type { H3Event } from "h3";
 
 const getUserRole = async (event: H3Event) => {
-	const accessToken = String(getCookie(event, "accessToken"));
+	const accessToken = getAccessToken(event);
 	const { secret } = useRuntimeConfig();
 	const { role } = await verify(accessToken, secret);
 	return role;
